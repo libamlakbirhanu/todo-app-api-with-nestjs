7364b0bff0094982d9206ea2a88909f15a96eb3b
Libamlak Birhanu <realweirdo904@gmail.com>
2021-11-23 11:40:40 +0300

    reading accessToken from the headers instead of cookies

--- src/strategies/jwt.strategy.ts
+++ src/strategies/jwt.strategy.ts
@@ -14,7 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
         (request: Request) => {
-          return request?.cookies?.accessToken;
+          return request?.headers?.authorization.split(' ')[1];
         },
       ]),
       ignoreExpiration: false,
