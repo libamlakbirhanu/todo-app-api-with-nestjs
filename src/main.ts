615585469a807c2d43cb86869b2f0b7296329bdd
Libamlak Birhanu <libamlakbirhanu904@gmail.com>
2022-03-08 02:13:41 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
     ],
     origin: 'http://localhost:3000',
   });
-  await app.listen(parseInt(process.env.PORT, '0.0.0.0') || 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
